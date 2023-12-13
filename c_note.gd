7252446d8c509790e332d6a28f1dd35a08c903da
paradoxniteowl <148103497+paradoxniteowl@users.noreply.github.com>
2023-12-13 11:14:29 -0600

    Not much progress this changing tilemap thing is killing me

--- c_note.gd
+++ c_note.gd
@@ -1,19 +1,14 @@
 extends Sprite2D
 
+
 var c_collected = false
-@onready var tilemap = $TileMap
+const WorldScript = preload("res://world.gd")
 
-func replace_floor():
-	var map_size = tilemap.get_map_size()
-	for x in range(map_size.x):
-		for y in range(map_size.y):
-			if tilemap.get_cell(x, y) == tilemap.tile_set.find_tile_by_name("block.png"):
-				tilemap.set_cell(x, Vector2i(x, y), tilemap.tile_set.find_tile_by_name("block2.png"))
-
+@onready var world_instance = WorldScript.new()
 
 func _on_c_collect_body_entered(body):
 	if body.is_in_group("Player") and not c_collected:
 		c_collected = true
 		$c_collect/AudioStreamPlayer2D.play()
-		replace_floor()
+		world_instance.replace_floor()
 		hide()
--- world.gd
+++ world.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+@onready var tilemap = get_node("TileMap")
+
+func replace_floor():
+	var used_rect = tilemap.get_used_rect()
+	for x in range(used_rect.size.x):
+		for y in range(used_rect.size.y):
+			if tilemap.get_cell(x, y) == tilemap.tile_set.find_tile_by_name("block.png"):
+				tilemap.set_cell(x, Vector2i(x, y), tilemap.tile_set.find_tile_by_name("block2.png"))
